d018331b2816ea29b95edbff6cf1de84935228f4
Emre19han03 <62987366+Emre19han03@users.noreply.github.com>
2025-10-29 23:29:37 +0300

    App

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+import { useState } from "react";
+import CourseInput from "./components/CourseInput";
 
 export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  const [courses, setCourses] = useState([]);
+
+  const startModal = () => {
+    setModalIsVisible(true);
+  };
+
+  const endModal = () => {
+    setModalIsVisible(false);
+  };
+  const addCourse = (courseTitle) => {
+    setCourses((currentCourses) => [
+      ...currentCourses,
+      {
+        text: courseTitle,
+        id: Math.random().toString(),
+      },
+    ]);
+    // console.log(courseTitle);
+    endModal();
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <Button title="Add Course" color="red" onPress={startModal} />
+        <CourseInput
+          visible={modalIsVisible}
+          onAddCourse={addCourse}
+          onCancel={endModal}
+        />
+        <View>
+          <FlatList
+            data={courses}
+            renderItem={({ item }) => (
+              <View style={styles.courseItem}>
+                <Text style={styles.courseText}>{item.text}</Text>
+              </View>
+            )}
+          />
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  courseItem: {
+    backgroundColor: "gray",
+    margin: 8,
+    borderRadius: 5,
+  },
+  courseText: {
+    padding: 8,
+    color: "white",
   },
 });
